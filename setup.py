d2c1cf59f3ac1d42af986ff344131d2279e89685
Mitsuhiro Setoguchi <setomits@gmail.com>
2024-05-20 21:11:58 +0900

    fix name and build tool

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="DPC Python",
+    name="dpc-python",
     version="0.1.0",
     description="A library for handling DPC data",
     long_description=open("README.md", "r", encoding="utf-8").read(),
